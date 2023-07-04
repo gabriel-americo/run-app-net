c2be3acaf85cd62c844b78b7baea171f644abc06
Gabriel Americo <gabrielamerico90@gmail.com>
2023-07-04 17:45:15 -0300

    Detail page

--- Controllers/ClubController.cs
+++ Controllers/ClubController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RunGroopWebApp.Data;
 using RunGroopWebApp.Models;
 
@@ -16,8 +17,13 @@ namespace RunGroopWebApp.Controllers
         public IActionResult Index()
         {
             List<Club> clubs = _context.Clubs.ToList();
-
             return View(clubs);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            Club club = _context.Clubs.Include(a => a.Address).FirstOrDefault(c => c.Id == id);
+            return View(club);
         }
     }
 }
